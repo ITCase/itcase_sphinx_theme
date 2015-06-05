235bcec948a0b221a494f3482071db3f184bae8b
Dmitry <root@uralbash.ru>
2015-06-05 13:34:32 +0500

    ++version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()

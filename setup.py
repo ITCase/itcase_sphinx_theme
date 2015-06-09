3994ab262c01fce6ab47792d385051c528f1fc0b
Dmitry <root@uralbash.ru>
2015-06-09 13:53:43 +0500

    rtd test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()

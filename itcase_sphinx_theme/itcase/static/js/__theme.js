854c2348d9b2cbcca3456a59be6c646f71f182b2
Arkadiy Zamaraev <arkadiy@bk.ru>
2015-07-14 12:56:16 +0600

    search result fix

--- itcase_sphinx_theme/itcase/static/js/__theme.js
+++ itcase_sphinx_theme/itcase/static/js/__theme.js
@@ -5,12 +5,25 @@ if (typeof $ === 'undefined') { require('jquery'); }
 if (typeof Cookies === 'undefined') {
   var Cookies = require('./vendor/js.cookie.js');
 }
+if (typeof sticky === 'undefined') {
+  var sticky = require('./vendor/jquery.sticky.js');
+}
 
 
 (function($){
+
   $(window).ready(function(){
 
     require('./vendor/jquery.fancybox.js');
+
+    if(STICKY_MENU) {
+      $('#menu').sticky({
+        topSpacing: 25,
+        bottomSpacing: 50,
+        getWidthFrom: '.page__left',
+        className: 'menu-sticky',
+      });
+    }
 
     $('.internal.image-reference').fancybox({
       padding : 3,
@@ -37,7 +50,7 @@ if (typeof Cookies === 'undefined') {
         pageRight = $('.page__right');
 
     function getTreeState() {
-      if(!document.cookie) { 
+      if(!document.cookie) {
         pageLeft.css({ visibility: 'visible' });
         pageRight.css({ visibility: 'visible' });
         return;
@@ -53,9 +66,9 @@ if (typeof Cookies === 'undefined') {
     }
 
     function collapseTree() {
-      menu.css({ height: menu.height()});
+      menu.css({ width: '12px', height: menu.height()});
       menu.data('state', 'collapse');
-      menu.addClass('menu__state_collapse');
+      menu.addClass('menu_state_collapse');
       pageLeft.addClass('page__left_state_collapse');
       pageRight.addClass('page__right_state_expand');
       menuSwitchArrow.text('»');
@@ -63,9 +76,9 @@ if (typeof Cookies === 'undefined') {
     }
 
     function expandTree() {
-      menu.css({ height: 'auto'});
+      menu.css({ width: '300px', height: 'auto'});
       menu.data('state', 'expand');
-      menu.removeClass('menu__state_collapse');
+      menu.removeClass('menu_state_collapse');
       pageLeft.removeClass('page__left_state_collapse');
       pageRight.removeClass('page__right_state_expand');
       menuSwitchArrow.text('«');
@@ -90,7 +103,7 @@ if (typeof Cookies === 'undefined') {
   });
 })(jQuery);
 
-},{"./vendor/jquery.fancybox.js":2,"./vendor/js.cookie.js":4,"jquery":3}],2:[function(require,module,exports){
+},{"./vendor/jquery.fancybox.js":2,"./vendor/jquery.sticky.js":4,"./vendor/js.cookie.js":5,"jquery":3}],2:[function(require,module,exports){
 /*!
  * fancyBox - jQuery Plugin
  * version: 2.1.5 (Fri, 14 Jun 2013)
@@ -11331,6 +11344,178 @@ return jQuery;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],4:[function(require,module,exports){
+// Sticky Plugin v1.0.0 for jQuery
+// =============
+// Author: Anthony Garand
+// Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
+// Improvements by Leonardo C. Daronco (daronco)
+// Created: 2/14/2011
+// Date: 2/12/2012
+// Website: http://labs.anthonygarand.com/sticky
+// Description: Makes an element on the page stick on the screen as you scroll
+//       It will only set the 'top' and 'position' of your element, you
+//       might need to adjust the width in some cases.
+
+(function($) {
+  var defaults = {
+      topSpacing: 0,
+      bottomSpacing: 0,
+      className: 'is-sticky',
+      wrapperClassName: 'sticky-wrapper',
+      center: false,
+      getWidthFrom: '',
+      responsiveWidth: false
+    },
+    $window = $(window),
+    $document = $(document),
+    sticked = [],
+    windowHeight = $window.height(),
+    scroller = function() {
+      var scrollTop = $window.scrollTop(),
+        documentHeight = $document.height(),
+        dwh = documentHeight - windowHeight,
+        extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
+
+      for (var i = 0; i < sticked.length; i++) {
+        var s = sticked[i],
+          elementTop = s.stickyWrapper.offset().top,
+          etse = elementTop - s.topSpacing - extra;
+
+        if (scrollTop <= etse) {
+          if (s.currentTop !== null) {
+            s.stickyElement
+              .css('position', '')
+              .css('top', '');
+            s.stickyElement.trigger('sticky-end', [s]).parent().removeClass(s.className);
+            s.currentTop = null;
+          }
+        }
+        else {
+          var newTop = documentHeight - s.stickyElement.outerHeight()
+            - s.topSpacing - s.bottomSpacing - scrollTop - extra;
+          if (newTop < 0) {
+            newTop = newTop + s.topSpacing;
+          } else {
+            newTop = s.topSpacing;
+          }
+          if (s.currentTop != newTop) {
+            s.stickyElement
+              .css('position', 'fixed')
+              .css('top', newTop);
+
+            if (typeof s.getWidthFrom !== 'undefined') {
+              s.stickyElement.css('width', $(s.getWidthFrom).width());
+            }
+
+            s.stickyElement.trigger('sticky-start', [s]).parent().addClass(s.className);
+            s.currentTop = newTop;
+          }
+        }
+      }
+    },
+    resizer = function() {
+      windowHeight = $window.height();
+
+      for (var i = 0; i < sticked.length; i++) {
+        var s = sticked[i];
+        if (typeof s.getWidthFrom !== 'undefined' && s.responsiveWidth === true) {
+          s.stickyElement.css('width', $(s.getWidthFrom).width());
+        }
+      }
+    },
+    methods = {
+      init: function(options) {
+        var o = $.extend({}, defaults, options);
+        return this.each(function() {
+          var stickyElement = $(this);
+
+          var stickyId = stickyElement.attr('id');
+          var wrapperId = stickyId ? stickyId + '-' + defaults.wrapperClassName : defaults.wrapperClassName 
+          var wrapper = $('<div></div>')
+            .attr('id', stickyId + '-sticky-wrapper')
+            .addClass(o.wrapperClassName);
+          stickyElement.wrapAll(wrapper);
+
+          if (o.center) {
+            stickyElement.parent().css({width:stickyElement.outerWidth(),marginLeft:"auto",marginRight:"auto"});
+          }
+
+          if (stickyElement.css("float") == "right") {
+            stickyElement.css({"float":"none"}).parent().css({"float":"right"});
+          }
+
+          var stickyWrapper = stickyElement.parent();
+          stickyWrapper.css('height', stickyElement.outerHeight());
+          sticked.push({
+            topSpacing: o.topSpacing,
+            bottomSpacing: o.bottomSpacing,
+            stickyElement: stickyElement,
+            currentTop: null,
+            stickyWrapper: stickyWrapper,
+            className: o.className,
+            getWidthFrom: o.getWidthFrom,
+            responsiveWidth: o.responsiveWidth
+          });
+        });
+      },
+      update: scroller,
+      unstick: function(options) {
+        return this.each(function() {
+          var unstickyElement = $(this);
+
+          var removeIdx = -1;
+          for (var i = 0; i < sticked.length; i++)
+          {
+            if (sticked[i].stickyElement.get(0) == unstickyElement.get(0))
+            {
+                removeIdx = i;
+            }
+          }
+          if(removeIdx != -1)
+          {
+            sticked.splice(removeIdx,1);
+            unstickyElement.unwrap();
+            unstickyElement.removeAttr('style');
+          }
+        });
+      }
+    };
+
+  // should be more efficient than using $window.scroll(scroller) and $window.resize(resizer):
+  if (window.addEventListener) {
+    window.addEventListener('scroll', scroller, false);
+    window.addEventListener('resize', resizer, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onscroll', scroller);
+    window.attachEvent('onresize', resizer);
+  }
+
+  $.fn.sticky = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
+  };
+
+  $.fn.unstick = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method ) {
+      return methods.unstick.apply( this, arguments );
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
+
+  };
+  $(function() {
+    setTimeout(scroller, 0);
+  });
+})(jQuery);
+
+},{}],5:[function(require,module,exports){
 /*!
  * JavaScript Cookie v2.0.2
  * https://github.com/js-cookie/js-cookie
--- itcase_sphinx_theme/itcase/static/js/main.js
+++ itcase_sphinx_theme/itcase/static/js/main.js
@@ -4,12 +4,25 @@ if (typeof $ === 'undefined') { require('jquery'); }
 if (typeof Cookies === 'undefined') {
   var Cookies = require('./vendor/js.cookie.js');
 }
+if (typeof sticky === 'undefined') {
+  var sticky = require('./vendor/jquery.sticky.js');
+}
 
 
 (function($){
+
   $(window).ready(function(){
 
     require('./vendor/jquery.fancybox.js');
+
+    if(STICKY_MENU) {
+      $('#menu').sticky({
+        topSpacing: 25,
+        bottomSpacing: 50,
+        getWidthFrom: '.page__left',
+        className: 'menu-sticky',
+      });
+    }
 
     $('.internal.image-reference').fancybox({
       padding : 3,
@@ -36,7 +49,7 @@ if (typeof Cookies === 'undefined') {
         pageRight = $('.page__right');
 
     function getTreeState() {
-      if(!document.cookie) { 
+      if(!document.cookie) {
         pageLeft.css({ visibility: 'visible' });
         pageRight.css({ visibility: 'visible' });
         return;
@@ -52,9 +65,9 @@ if (typeof Cookies === 'undefined') {
     }
 
     function collapseTree() {
-      menu.css({ height: menu.height()});
+      menu.css({ width: '12px', height: menu.height()});
       menu.data('state', 'collapse');
-      menu.addClass('menu__state_collapse');
+      menu.addClass('menu_state_collapse');
       pageLeft.addClass('page__left_state_collapse');
       pageRight.addClass('page__right_state_expand');
       menuSwitchArrow.text('»');
@@ -62,9 +75,9 @@ if (typeof Cookies === 'undefined') {
     }
 
     function expandTree() {
-      menu.css({ height: 'auto'});
+      menu.css({ width: '300px', height: 'auto'});
       menu.data('state', 'expand');
-      menu.removeClass('menu__state_collapse');
+      menu.removeClass('menu_state_collapse');
       pageLeft.removeClass('page__left_state_collapse');
       pageRight.removeClass('page__right_state_expand');
       menuSwitchArrow.text('«');
